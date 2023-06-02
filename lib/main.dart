aa7e77c29ce10263675aefe92dbae40f37339b0a
Edwin Cardenas <eacardenase@unal.edu.co>
2023-06-01 21:20:40 -0500

    refactor: extract container into custom widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,28 +3,35 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Colors.cyan,
-              Colors.pink,
-            ],
-            begin: Alignment.topRight,
-            end: Alignment.bottomLeft,
-          ),
+      body: GradientContainer(),
+    ),
+  ));
+}
+
+class GradientContainer extends StatelessWidget {
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Colors.cyan,
+            Colors.pink,
+          ],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
         ),
-        child: const Center(
-          child: Text(
-            'Hello from Flutter!',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 28,
-              fontWeight: FontWeight.bold,
-            ),
+      ),
+      child: const Center(
+        child: Text(
+          'Hello from Flutter!',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+            fontWeight: FontWeight.bold,
           ),
         ),
       ),
-    ),
-  ));
+    );
+  }
 }
